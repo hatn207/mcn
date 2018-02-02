6a68f4b2956f54388cc387288eae56a122c9b9e4
GLODEIT\Ha.TN <ha.tn@globaldesignit.vn>
2018-02-02 10:06:13 +0700

    soure code

---
+++ admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/mcn/admin/');
+define('HTTP_CATALOG', 'http://localhost/mcn/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/mcn/admin/');
+define('HTTPS_CATALOG', 'http://localhost/mcn/');
+
+// DIR
+define('DIR_APPLICATION', 'E:\xam\htdocs\mcn/admin/');
+define('DIR_SYSTEM', 'E:\xam\htdocs\mcn/system/');
+define('DIR_LANGUAGE', 'E:\xam\htdocs\mcn/admin/language/');
+define('DIR_TEMPLATE', 'E:\xam\htdocs\mcn/admin/view/template/');
+define('DIR_CONFIG', 'E:\xam\htdocs\mcn/system/config/');
+define('DIR_IMAGE', 'E:\xam\htdocs\mcn/image/');
+define('DIR_CACHE', 'E:\xam\htdocs\mcn/system/cache/');
+define('DIR_DOWNLOAD', 'E:\xam\htdocs\mcn/system/download/');
+define('DIR_UPLOAD', 'E:\xam\htdocs\mcn/system/upload/');
+define('DIR_LOGS', 'E:\xam\htdocs\mcn/system/logs/');
+define('DIR_MODIFICATION', 'E:\xam\htdocs\mcn/system/modification/');
+define('DIR_CATALOG', 'E:\xam\htdocs\mcn/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'mcn');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/mcn/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/mcn/');
+
+// DIR
+define('DIR_APPLICATION', 'E:\xam\htdocs\mcn/catalog/');
+define('DIR_SYSTEM', 'E:\xam\htdocs\mcn/system/');
+define('DIR_LANGUAGE', 'E:\xam\htdocs\mcn/catalog/language/');
+define('DIR_TEMPLATE', 'E:\xam\htdocs\mcn/catalog/view/theme/');
+define('DIR_CONFIG', 'E:\xam\htdocs\mcn/system/config/');
+define('DIR_IMAGE', 'E:\xam\htdocs\mcn/image/');
+define('DIR_CACHE', 'E:\xam\htdocs\mcn/system/cache/');
+define('DIR_DOWNLOAD', 'E:\xam\htdocs\mcn/system/download/');
+define('DIR_UPLOAD', 'E:\xam\htdocs\mcn/system/upload/');
+define('DIR_MODIFICATION', 'E:\xam\htdocs\mcn/system/modification/');
+define('DIR_LOGS', 'E:\xam\htdocs\mcn/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'mcn');
+define('DB_PREFIX', 'oc_');
